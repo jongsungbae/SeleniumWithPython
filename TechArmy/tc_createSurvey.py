715a8c8b91f4045e3aa45d48b56d6a360fe27877
jongsungbae <jongsung.jaybae@gmail.com>
2022-04-12 19:41:43 -0400

    testcase for create survey

--- TechArmy/tc_createSurvey.py
+++ TechArmy/tc_createSurvey.py
@@ -2,9 +2,11 @@ import time
 import unittest, xlrd
 from ddt import ddt, data, unpack
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
 
@@ -22,15 +24,43 @@ class SignInTest(unittest.TestCase):
 
     @data(('test01', 'test01'))
     @unpack
-    def test_createSurvey_move(self, username, password):
-        self.driver.find_element(By.LINK_TEXT, "Create Survey").click()
-        self.driver.find_element(By.NAME, 'username').send_keys(username)
-        self.driver.find_element(By.NAME, 'password').send_keys(password)
-        time.sleep(3)
-        self.driver.find_element(By.CLASS_NAME, 'signinBtn').click()
+    def test_A_createSurvey_move(self,username,password):
+        self.driver.find_element(By.LINK_TEXT, "Sign In").click()
+        username1 = self.driver.find_element(By.NAME, 'username')
+        password1 = self.driver.find_element(By.NAME, 'password')
+        signInBtn = self.driver.find_element(By.CLASS_NAME, 'signinBtn')
+
+        username1.send_keys(username)
+        password1.send_keys(password)
+        self.assertTrue(signInBtn.is_enabled())
+        signInBtn.send_keys("\n")
 
         WebDriverWait(self.driver, 10).until(expected_conditions.title_contains("Home"))
         self.driver.find_element(By.LINK_TEXT, "Create Survey").click()
+        self.assertTrue('Survey', self.driver.title)
+
+    def test_B_creteSurvey(self):
+        self.driver.find_element(By.NAME, 'startDate').send_keys('03312022')
+        self.driver.find_element(By.NAME, 'endDate').send_keys('04302022')
+        self.driver.find_element(By.ID, 'surveyTextField').send_keys('test survey for automation')
+        self.driver.find_element(By.ID, 'addQues').click()
+        self.driver.find_element(By.NAME, 'question1').send_keys('question01 for automation')
+
+        exp_options = ['Select Type', 'Single Textbox', 'Multiple Choice']
+        act_options = []
+        select_list = Select(self.driver.find_element(By.NAME, 'question1type'))
+        # check number of options in dropdown
+        self.assertEqual(3, len(select_list.options))
+        # get options in a list
+        for option in select_list.options:
+            act_options.append(option.text)
+        # check expected options list with actual options list
+        self.assertEqual(exp_options, act_options)
+        # check default selected option is "Select Type"
+        self.assertEqual("Select Type", select_list.first_selected_option.text)
+
+        select_list.select_by_visible_text('Single Textbox')
+        self.driver.find_element(By.ID, 'createSurvey').send_keys("\n")
 
     @classmethod
     def tearDownClass(cls):
