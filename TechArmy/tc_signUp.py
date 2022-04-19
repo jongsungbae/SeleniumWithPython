f65a238fa8e6e73fbbfe16fb71dc6f4d999b1649
jongsungbae <90482015+jongsungbae@users.noreply.github.com>
2022-04-18 23:04:27 -0400

    create project

--- TechArmy/tc_signUp.py
+++ TechArmy/tc_signUp.py
@@ -1,55 +1,30 @@
-import unittest, time
+import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-class SignUpTest(unittest.TestCase):
+
+class PfcUnitTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        baseUrl = "https://tech-army-survey.herokuapp.com/"
-        s = Service('E:\\selenium\\drivers\\chromedriver.exe')
+        baseUrl = "https://pfconenet.herokuapp.com/"
+        s = Service('D:\\PFCOneNet\\test\\chromedriver.exe')
         cls.driver = webdriver.Chrome(service=s)
         cls.driver.implicitly_wait(30)
         cls.driver.maximize_window()
         cls.driver.get(baseUrl)
 
-    def test_check_A_cancelBtn(self):
-        driver = self.driver
-        driver.find_element(By.LINK_TEXT, "Sign Up").click()
-        self.assertEqual("Sign Up", driver.title)
-        cancelBtn = self.driver.find_element(By.ID, 'registerCancel')
-        self.assertTrue(cancelBtn.is_enabled())
-        cancelBtn.click()
-        self.assertEqual('Home', driver.title)
-        driver.find_element(By.LINK_TEXT, "Sign Up").click()
+    def setUp(self):
+        logInBtn = self.driver.find_element(By.LINK_TEXT, 'Log in')
+        logInBtn.click()
+        self.driver.find_element(By.NAME, "username").send_keys('PFCUser7')
+        self.driver.find_element(By.NAME, "password").send_keys('PFCPass7')
+        self.driver.find_element(By.XPATH, "/html/body/div/main/div/form/input[3]").click()
 
-    def test_check_B_linkBtn(self):
-        driver = self.driver
-        signInLink = self.driver.find_element(By.LINK_TEXT, 'or Sign in here')
-        self.assertTrue(signInLink.is_displayed())
-        signInLink.click()
-        self.assertEqual('Sign In', driver.title)
-        driver.back()
-        self.assertEqual('Sign Up', driver.title)
-
-    def test_create_account(self):
-        name = self.driver.find_element(By.NAME, 'username')
-        password = self.driver.find_element(By.NAME, 'password')
-        email = self.driver.find_element(By.NAME, 'email')
-        createBtn = self.driver.find_element(By.ID, 'registerCreate')
-        self.assertTrue(name.is_displayed())
-        self.assertTrue(password.is_displayed())
-        self.assertTrue(email.is_displayed())
-        self.assertTrue(createBtn.is_enabled())
-
-        name.send_keys('test999')
-        password.send_keys('test999')
-        email.send_keys('test999@test.com')
-        createBtn.click()
-        time.sleep(2)
-
-        checkID = self.driver.find_element(By.ID, 'dropbtn').text
-        self.assertEqual('test999', checkID)
+    def test_unittest1(self):
+        welcomMsg = self.driver.find_element(By.CLASS_NAME, "nav-text").text
+        self.assertEqual('Welcome: PFCUser7', welcomMsg)
+        print("unittest_login_success")
 
     @classmethod
     def tearDownClass(cls):
@@ -58,3 +33,6 @@ class SignUpTest(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+
+
