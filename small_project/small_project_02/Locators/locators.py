7a6824c97e7f6885c648b3b2f5c16cb66225d8c1
jongsungbae <jongsung.jaybae@gmail.com>
2022-08-23 15:02:38 -0400

    login test in small project2

---
+++ small_project/small_project_02/Locators/locators.py
@@ -0,0 +1,10 @@
+class Locators():
+
+    # Login page objects
+    username_textbox_name = "username"
+    password_textbox_name = "password"
+    login_button_CSSSELECTOR = 'button[type="submit"]'
+
+    # Home page objects
+    welcome_dropdown_CSSSELECTOR = "span[class='oxd-userdropdown-tab']"
+    logout_linktext = "Logout"
---
+++ small_project/small_project_02/Tests/login.py
@@ -0,0 +1,43 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import unittest
+import HtmlTestRunner
+from small_project_02.Pages.homePage import HomePage
+from small_project_02.Pages.loginPage import LoginPage
+
+class LoginTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        baseUrl= 'https://opensource-demo.orangehrmlive.com/'
+        s = Service('../../driver/chromedriver.exe')
+
+        cls.driver = webdriver.Chrome(service=s)
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
+        cls.driver.get(baseUrl)
+
+    def test_login_page(self):
+        driver = self.driver
+
+        login = LoginPage(driver)
+        login.enter_username("Admin")
+        login.enter_password("admin123")
+        login.click_login()
+
+    def test_logout(self):
+        driver = self.driver
+        logout = HomePage(driver)
+        logout.click_welcome()
+        logout.click_logout()
+
+        time.sleep(2)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.driver.quit()
+        print('Test Completed')
+
+if __name__ == "__main__":
+    unittest.main()
