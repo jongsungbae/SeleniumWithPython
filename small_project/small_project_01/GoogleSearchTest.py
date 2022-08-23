16e14e596450caacb44307e60eaaeccd5b515ab6
jongsungbae <jongsung.jaybae@gmail.com>
2022-08-23 13:43:12 -0400

    small project 1 restructure

---
+++ small_project/small_project_01/GoogleSearchTest.py
@@ -0,0 +1,38 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import unittest
+import HtmlTestRunner
+
+class GoogleSearchTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        baseUrl= 'https://google.com'
+        s = Service('driver/chromedriver.exe')
+
+        cls.driver = webdriver.Chrome(service=s)
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
+        cls.driver.get(baseUrl)
+
+    def test_search_keyword_01(self):
+        global search_bar, search_button
+        self.driver.find_element(By.NAME, 'q').send_keys("Automation step by step")
+        self.driver.find_element(By.NAME, 'btnK').click()
+        time.sleep(2)
+
+    def test_search_keyword_02(self):
+        self.driver.find_element(By.NAME, 'q').clear()
+        time.sleep(2)
+        self.driver.find_element(By.NAME, 'q').send_keys("Selenium")
+        self.driver.find_element(By.CSS_SELECTOR, 'button[type=submit').click()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.driver.quit()
+        print('Test Completed')
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ small_project/small_project_01/generate_report.py
@@ -0,0 +1,20 @@
+import os, unittest
+from HtmlTestRunner import HTMLTestRunner
+from GoogleSearchTest import GoogleSearchTest
+
+# get the directory path to output report file
+dir = os.getcwd()
+
+# get all tests from search Test and homepage Test
+google_search_test = unittest.TestLoader().loadTestsFromTestCase(GoogleSearchTest)
+
+# create a test suite combining search_test and homepage test
+smoke_tests = unittest.TestSuite([google_search_test])
+
+# configure HTMLTestRunner options
+runner = HTMLTestRunner(
+    output='reports'
+)
+
+# Run the suite using HTMLTestRunner
+runner.run(smoke_tests)
